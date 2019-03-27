85d0b1b49e269be03cdd708724bf803ae2b419d3
Dmitrenko <Dias64@mail.ru>
2019-03-27 13:39:52 +0300

    Код с урока

---
+++ src/ru/geekbrains/lesson1/Participant.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson1;
+
+/**
+ * Интерфейс - участник соревнований
+ */
+public interface Participant {
+    boolean isOnDistance();
+
+    void run(int distance);
+
+    void jump(int height);
+
+    void swim(int distance);
+}
