28f39a570ab6156b13af9cd990f25b7389be552b
Dmitrenko <Dias64@mail.ru>
2019-04-19 11:37:11 +0300

    homework6

--- src/ru/geekbrains/lesson6/EchoClient.java
+++ src/ru/geekbrains/lesson6/EchoClient.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.lesson6;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -7,21 +8,52 @@ import java.util.Scanner;
 
 public class EchoClient {
 
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 7777;
+
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(System.in);
-             Socket socket = new Socket("localhost", 7777)) {
-
+        try (Socket socket = new Socket(HOST, PORT)) {
+            // канал чтения из сокета
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            // канал записи в сокет
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            System.out.print("Введите сообщение > ");
-            while (scanner.hasNextLine()) {
-                System.out.print("Введите сообщение > ");
-                String line = scanner.nextLine();
-                out.writeUTF(line);
+            String inputMessage;
+
+            new Thread(new Runnable() {
+                @Override
+                public void run () {
+                    try (Scanner scanner = new Scanner(System.in)) {
+                        while (!Thread.currentThread().isInterrupted() && scanner.hasNextLine()) {
+                            System.out.println("Введите сообщение > ");
+                            String outputMessage = scanner.nextLine();
+                            try {
+                                out.writeUTF(outputMessage);
+                                out.flush();
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }).start();
+
+            while (!socket.isOutputShutdown()) {
+                System.out.println("Введите сообщение > ");
+                try {
+                    while (true) {
+                        inputMessage = in.readUTF();
+                        System.out.println("Новое сообщение > " + inputMessage);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    break;
+                }
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
+
 }
--- src/ru/geekbrains/lesson6/EchoServer.java
+++ src/ru/geekbrains/lesson6/EchoServer.java
@@ -5,27 +5,53 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoServer {
 
+    private static final int PORT = 7777;
+
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(7777)) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Сервер ожидает подключения!");
             Socket socket = serverSocket.accept();
-            System.out.println("Кто-то подключился: " + socket.getInetAddress());
-
+            System.out.println("Кто-то подключился: "+socket.getInetAddress());
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            while (true) {
+            String inputMessage;
+
+            new Thread(new Runnable() {
+                @Override
+                public void run () {
+                    try (Scanner scanner = new Scanner(System.in)) {
+                        while (!Thread.currentThread().isInterrupted() && scanner.hasNextLine()) {
+                            System.out.println("Введите сообщение > ");
+                            String outputMessage = scanner.nextLine();
+                            try {
+                                out.writeUTF(outputMessage);
+                                out.flush();
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }).start();
+
+            while(socket.isConnected()) {
+                System.out.println("Введите сообщение > ");
                 try {
-                    System.out.println("Новое сообщение > " + in.readUTF());
-                } catch (IOException ex) {
+                    while (true) {
+                        inputMessage = in.readUTF();
+                        System.out.println("Новое сообщение > " + inputMessage);
+                    }
+                } catch(IOException ex) {
                     ex.printStackTrace();
                     break;
                 }
             }
-        } catch (IOException ex) {
+        } catch(IOException ex) {
             ex.printStackTrace();
         }
     }
